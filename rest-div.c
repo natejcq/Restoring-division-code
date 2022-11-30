bfd37a1c52b36e2b973db6c949d8fecd807bc97a
natejcq <113688841+natejcq@users.noreply.github.com>
2022-12-01 01:02:30 +0530

    Update rest-div.c
    decimal to binary with wrap(extending range with zeros)

--- rest-div.c
+++ rest-div.c
@@ -2,6 +2,25 @@
 #include<stdlib.h>
 #include<math.h>
 
+int dec_bin(int d, int m[], int digits)
+{
+    int b=0, i=0;
+    while(d>0)
+    {
+        m[i]=d%2;
+        printf("%d", d%2);
+        d=d/2;
+        i++;
+    }
+    printf(" i is %d\n", i);
+    while(i<digits)
+    {
+        m[i] = 0;
+        i++;
+    }
+    return 0;
+}
+
 void reverse_arr(int arr[], int digits)
 {
     int temp;
