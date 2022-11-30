35fe38ca9fe8ae5f5672f01e8508712dc486f2f7
natejcq <113688841+natejcq@users.noreply.github.com>
2022-12-01 00:59:35 +0530

    Update rest-div.c
    basic reading input, calculating number of bits

--- rest-div.c
+++ rest-div.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include<math.h>
 
+int main()
+{
+    int a,b;
+    printf("Enter Dividend:");
+    scanf("%d", &a);
+    printf("Enter the Divisor:");
+    scanf("%d", &b);
+    //Finding greater number
+    int maximum = abs(a)>abs(b) ? abs(a) : abs(b);
+    //Finding number of digits
+    int digits = (log(maximum)/log(2)) + 1;
+    printf("digits is %d \n", digits);
+}
+   
